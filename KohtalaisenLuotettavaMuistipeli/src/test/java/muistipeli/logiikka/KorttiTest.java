24dd2b4332353ee386a8e999cb3ad82b7575f313
Olli Tanttu <otanttu@cs.helsinki.fi>
2017-01-27 13:31:30 +0200

    Luokka testit eriytetty omiin testiluokkiinsa

--- KohtalaisenLuotettavaMuistipeli/src/test/java/muistipeli/logiikka/KorttiTest.java
+++ KohtalaisenLuotettavaMuistipeli/src/test/java/muistipeli/logiikka/KorttiTest.java
@@ -18,62 +18,15 @@ import static org.junit.Assert.*;
  *
  * @author otanttu
  */
-public class LogiikkaTest {
+public class KorttiTest {
 
-    public LogiikkaTest() {
+    public KorttiTest() {
     }
 
     @Before
     public void setUp() {
         Peli peli = new Peli();
         peli.luoKortit();
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void testToimiikoPelaajanNimiOikein() {
-        Pelaaja p = new Pelaaja("Matti");
-        assertEquals("Matti", p.getNimi());
-    }
-
-    @Test
-    public void testPelaajanParitAlussaNolla() {
-        Pelaaja p = new Pelaaja("Matti");
-        assertEquals(0, p.getParit());
-    }
-
-    @Test
-    public void testYksiLoydettyPariLisaaParienLkm() {
-        Pelaaja p = new Pelaaja("Matti");
-        p.loysiParin();
-        assertEquals(1, p.getParit());
-    }
-
-    @Test
-    public void testUseampiLoydettyPariLisaaParienLkmOikein() {
-        Pelaaja p = new Pelaaja("Matti");
-        p.loysiParin();
-        p.loysiParin();
-        p.loysiParin();
-        assertEquals(3, p.getParit());
-    }
-
-    @Test
-    public void testToStringToimiiOikeinOikeinYhdellaParilla() {
-        Pelaaja p = new Pelaaja("Matti");
-        p.loysiParin();
-        assertEquals("Matti: 1 pari", p.toString());
-    }
-
-    @Test
-    public void testToStringToimiiOikeinUseammallaParilla() {
-        Pelaaja p = new Pelaaja("Matti");
-        p.loysiParin();
-        p.loysiParin();
-        assertEquals("Matti: 2 paria", p.toString());
     }
 
     @Test
@@ -94,27 +47,27 @@ public class LogiikkaTest {
         Kortti l = new Kortti("kana");
         assertEquals(true, k.equals(l));
     }
-    
+
     @Test
     public void testKorttieEqualsToimiiJosVerrattavaEiObjekti() {
         Kortti k = new Kortti("kana");
         assertEquals(false, k.equals(0));
     }
-    
+
     @Test
     public void testKorttiEqualsToimiiJosVerrattavaNull() {
         Kortti k = new Kortti("kana");
         Kortti l = null;
         assertEquals(false, k.equals(l));
     }
-    
+
     @Test
     public void testKorttiEqualsToimiiJosEriLuokka() {
         Kortti k = new Kortti("kana");
         String l = "moi";
         assertEquals(false, k.equals(l));
     }
-    
+
     @Test
     public void testKorttiEqualsToimiiJosEriTunnus() {
         Kortti k = new Kortti("kana");
@@ -129,9 +82,4 @@ public class LogiikkaTest {
         assertEquals(k.hashCode(), l.hashCode());
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
---
+++ KohtalaisenLuotettavaMuistipeli/src/test/java/muistipeli/logiikka/PelaajaTest.java
@@ -0,0 +1,63 @@
+package muistipeli.logiikka;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PelaajaTest {
+
+    public PelaajaTest() {
+    }
+
+    @Before
+    public void setUp() {
+        Peli peli = new Peli();
+        peli.luoKortit();
+    }
+
+    @Test
+    public void testToimiikoPelaajanNimiOikein() {
+        Pelaaja p = new Pelaaja("Matti");
+        assertEquals("Matti", p.getNimi());
+    }
+
+    @Test
+    public void testPelaajanParitAlussaNolla() {
+        Pelaaja p = new Pelaaja("Matti");
+        assertEquals(0, p.getParit());
+    }
+
+    @Test
+    public void testYksiLoydettyPariLisaaParienLkm() {
+        Pelaaja p = new Pelaaja("Matti");
+        p.loysiParin();
+        assertEquals(1, p.getParit());
+    }
+
+    @Test
+    public void testUseampiLoydettyPariLisaaParienLkmOikein() {
+        Pelaaja p = new Pelaaja("Matti");
+        p.loysiParin();
+        p.loysiParin();
+        p.loysiParin();
+        assertEquals(3, p.getParit());
+    }
+
+    @Test
+    public void testToStringToimiiOikeinOikeinYhdellaParilla() {
+        Pelaaja p = new Pelaaja("Matti");
+        p.loysiParin();
+        assertEquals("Matti: 1 pari", p.toString());
+    }
+
+    @Test
+    public void testToStringToimiiOikeinUseammallaParilla() {
+        Pelaaja p = new Pelaaja("Matti");
+        p.loysiParin();
+        p.loysiParin();
+        assertEquals("Matti: 2 paria", p.toString());
+    }
+}
